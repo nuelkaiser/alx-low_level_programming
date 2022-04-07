81c7bdce41875242c8143a999d6ff468156c60c7
nuelkaiser <majekodunmiimmanuel9@gmail.com>
2022-04-07 14:46:33 -0700

    first c program written stating: programming is like building a  multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: using the main function
+ *  this program prints "Programming is like bulding a multolingual puzzle"
+ *  Return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
