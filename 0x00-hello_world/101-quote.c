4f739df387c459452e98c74accd12ad8f4bb0720
nuelkaiser <majekodunmiimmanuel9@gmail.com>
2022-04-07 17:22:59 -0700

    task quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - prints a quote
- * Return: (1);
+
+/**
+ * main - Prints a quote
+ *
+ * Return: Alaways 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
