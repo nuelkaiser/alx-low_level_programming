602ddc5d0088b2dc70f3294dc0b42d3fefb1b054
nuelkaiser <majekodunmiimmanuel9@gmail.com>
2022-04-27 03:38:23 -0700

    Task 4

--- 0x07-pointers_arrays_strings/strpbrk.c
+++ 0x07-pointers_arrays_strings/strpbrk.c
@@ -1,20 +1,24 @@
-
 #include "main.h"
 
 /**
- * _strpbrk - finds first matching char in string
- * @s: input string to search for matching char
- * @accept: characters that could be matched
- * Return: pointer to matching char
+ * _strspn - gets the length of a prefix substring
+ * @s: input string to search for substring
+ * @accept: characters that prefix substring must include
+ * Return: length of prefix substring
  */
 
-char *_strpbrk(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, a_len = 0, len = 0;
 
+	while (accept[a_len] != '\0')
+		a_len++;
 	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; j < a_len; j++)
 			if (s[i] == accept[j])
-				goto exit;
-exit: return (s[i] != '\0' ? s + i : '\0');
+				len++, j = a_len;
+			else
+				if (j == a_len - 1)
+					goto exit;
+exit: return (len);
 }
